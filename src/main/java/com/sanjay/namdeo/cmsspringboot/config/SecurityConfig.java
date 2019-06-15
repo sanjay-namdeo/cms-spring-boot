0dc848566bb75b098b03671e8c6f3f6529f06569
Sanjay Namdeo <sanjay.namdeo@live.com>
2019-06-15 23:43:48 +0200

    Postman configuration

--- src/main/java/com/sanjay/namdeo/cmsspringboot/config/SecurityConfig.java
+++ src/main/java/com/sanjay/namdeo/cmsspringboot/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.sanjay.namdeo.cmsspringboot.config;
 
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -21,8 +23,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
         User.UserBuilder userBuilder = User.builder().passwordEncoder(encoder::encode);
 
-        UserDetails testUser = userBuilder.username("test").password("test").roles("USER", "ADMIN").build();
+        UserDetails testUser = userBuilder.username("test").password("test").roles("USER").build();
+        UserDetails adminUser = userBuilder.username("admin").password("admin").roles("ADMIN").build();
 
-        return new InMemoryUserDetailsManager(testUser);
+
+        return new InMemoryUserDetailsManager(testUser, adminUser);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().and().authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/customers/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET, "/customers/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/customers/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/customers/**").hasRole("ADMIN")
+                .and().csrf().disable().headers().frameOptions().disable();
     }
 }
